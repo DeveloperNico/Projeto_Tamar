c504e1dd68ff5b02b3b3ecd8811978b013d4b74d
DeveloperNico <nickboytube0418@gmail.com>
2025-10-09 21:32:54 -0300

    ...

--- src/Components/BeachSection/BeachSection.jsx
+++ src/Components/BeachSection/BeachSection.jsx
@@ -1,22 +1,41 @@
 import styles from './BeachSection.module.css';
 import ondaGif from '../../assets/Icons/onda.gif';
+import tartaruga from '../../assets/Icons/tartaruga.png';
 
 export function BeachSection() {
   return (
     <section className={styles.container}>
-        {/* Areia */}
-        <div className={styles.sand}>
-            {/* Mar animado */}
-            <img src={ondaGif} alt="Gif de onda" />
-            <img src={ondaGif} alt="Gif de onda" />
-            <img src={ondaGif} alt="Gif de onda" />
+      {/* Areia */}
+      <div className={styles.sand}>
+        <img src={ondaGif} alt="Gif de onda" />
+        <img src={ondaGif} alt="Gif de onda" />
+        <img src={ondaGif} alt="Gif de onda" />
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.turtleGroup}>
+          <div className={styles.turtleLeft}>
+            <img src={tartaruga} alt="Tartaruga filhote esquerda" />
+          </div>
+
+          <div className={styles.turtleCenter}>
+            <img src={tartaruga} alt="Tartaruga principal" />
+          </div>
+
+          <div className={styles.turtleRight}>
+            <img src={tartaruga} alt="Tartaruga filhote direita" />
+          </div>
         </div>
 
-        <div className={styles.content}>
-            <p>A Fundação Projeto Tamar atua no litoral brasileiro desde a década de 80 com a missão de promover a recuperação das tartarugas marinhas, através de ações de pesquisa, conservação e inclusão social.
-
-É uma entidade de direito privado, sem fins lucrativos e co-executora do PAN - Plano Nacional de Ação para a Conservação das Tartarugas Marinhas no Brasil do ICMBio/MMA, sendo responsável por grande parte das ações previstas.</p>
+        <div>
+          <p>
+            A Fundação Projeto Tamar atua no litoral brasileiro desde a década de 80 com a missão de promover a recuperação das tartarugas marinhas, através de ações de pesquisa, conservação e inclusão social.
+          </p>
+          <p>
+            É uma entidade de direito privado, sem fins lucrativos e co-executora do PAN - Plano Nacional de Ação para a Conservação das Tartarugas Marinhas no Brasil do ICMBio/MMA, sendo responsável por grande parte das ações previstas.
+          </p>
         </div>
+      </div>
     </section>
   );
 }
