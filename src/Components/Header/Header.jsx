97c8bb47c2914358a17f680c2a8d0fcb35dd0a4b
DeveloperNico <nickboytube0418@gmail.com>
2025-10-23 16:34:12 -0300

    ...

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,35 @@
+import { useEffect, useState } from 'react';
 import styles from './Header.module.css';
 import logo from '../../assets/Icons/Logo.png';
 
 export function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setScrolled(window.scrollY > 50);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <header>
+    <header className={`${styles.header} ${scrolled ? styles.scrolled : ''}`}>
       <section>
-        <img src={logo} alt="Logotipo do Projeto Tamar com uma tartaruga marinha estilizada, sobre um fundo simples, transmitindo um tom calmo e acolhedor" />
+        <img
+          src={logo}
+          alt="Logotipo do Projeto Tamar com uma tartaruga marinha estilizada, sobre um fundo simples, transmitindo um tom calmo e acolhedor"
+        />
       </section>
 
       <nav>
         <ul className={styles.navList}>
-          <li><a href="#home">Ínicio</a></li>
+          <li><a href="#home">Início</a></li>
           <li><a href="#sobre">Sobre nós</a></li>
           <li><a href="#contato">Contato</a></li>
         </ul>
       </nav>
-    </header> 
+    </header>
   );
-};
+}
